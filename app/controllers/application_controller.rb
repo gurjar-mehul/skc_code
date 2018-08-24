07d5661ac6539e8c2a6dc374f183a0c234a29152
Mehul Gurjar <gurjar.mehul@gmail.com>
2018-08-25 01:16:26 +0530

    Name text field added in User edit form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def per_page
     params[:per_page] || 10
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
 
   def page_count
     params[:page] || 1
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
