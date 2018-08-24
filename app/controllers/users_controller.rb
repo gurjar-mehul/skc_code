1a2d5923db42b9a462a2b338b2d5291d7c3238cc
Mehul Gurjar <gurjar.mehul@gmail.com>
2018-08-25 00:41:17 +0530

    Destroy method defined for User controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :admin_only, :except => :show
+  before_action :admin_only, except: :show
+  before_action :find_user, only: [:show, :update, :destroy]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
     unless current_user.admin?
       unless @user == current_user
         redirect_to root_path, alert: 'Access denied.'
@@ -16,12 +16,16 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       redirect_to users_path, notice: 'User updated.'
     else
       redirect_to users_path, alert:  'Unable to update user.'
     end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: 'User destroyed successfully.'
   end
 
   private
@@ -34,5 +38,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:role)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
